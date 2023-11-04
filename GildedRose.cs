d010f2f84e19d0dbb4794c1ae1930cebe4c51547
m.anikin <m.anikin@intetics.com>
2023-11-04 15:51:21 +0100

    Making updates to the main switch. Combining all condition together in the switch.

--- GildedRose.cs
+++ GildedRose.cs
@@ -23,47 +23,44 @@ namespace csharp
         private void UpdateItemQuality(Item item)
         {
             if (item == null) throw new ArgumentNullException(nameof(item));
-
+            
+            if (item.Name != Constants.SulfurasHandOfRagnaros)
+                item.SellIn -= 1;
+            
             switch (item.Name)
             {
                 case Constants.SulfurasHandOfRagnaros:
                     // SulfurasHandOfRagnaros is legendary and does not lose it's quality
                     break;
                 case Constants.AgedBrie:
-                    item.ChangeQuality(1);
+                    item.ChangeQuality(item.SellIn < 0 ? 2 : 1);
                     break;
 
                 case Constants.BackstagePassesToATafkal80EtcConcert:
-                    item.ChangeQuality(1);
-
-                    if (item.SellIn < 11)
-                        item.ChangeQuality(1);
-
-                    if (item.SellIn < 6)
-                        item.ChangeQuality(1);
-
+                    switch (item.SellIn)
+                    {
+                        // drop quality after sell in
+                        case < 0:
+                            item.ChangeQuality(-1 * item.Quality);
+                            break;
+                        // 5 days before 
+                        case < 5:
+                            item.ChangeQuality(3);
+                            break;
+                        // 10 days before
+                        case < 10:
+                            item.ChangeQuality(2);
+                            break;
+                        // usual increase
+                        default:
+                            item.ChangeQuality(1);
+                            break;
+                    }
                     break;
                 default:
-                    item.ChangeQuality(-1);
+                    item.ChangeQuality(item.SellIn < 0 ? -2 : -1);
                     break;
             }
-
-            if (item.Name != Constants.SulfurasHandOfRagnaros)
-                item.SellIn -= 1;
-
-            if (item.SellIn < 0)
-                if (item.Name != Constants.AgedBrie)
-                {
-                    if (item.Name != Constants.BackstagePassesToATafkal80EtcConcert)
-                    {
-                        if (item.Name != Constants.SulfurasHandOfRagnaros)
-                            item.ChangeQuality(-1);
-                    }
-                    else
-                        item.ChangeQuality(-1 * item.Quality);
-                }
-                else
-                    item.ChangeQuality(1);
         }
     }
 }
