cd01a3bae2cf5257f71b83854c9550b310cdfdad
m.anikin <m.anikin@intetics.com>
2023-11-04 15:22:41 +0100

    Adding item extensions method so additional conditions are in the extension method, not in the main.

--- GildedRose.cs
+++ GildedRose.cs
@@ -6,7 +6,7 @@ namespace csharp
     public class GildedRose
     {
         IList<Item> Items;
-        
+
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -23,48 +23,42 @@ namespace csharp
         private void UpdateItemQuality(Item item)
         {
             if (item == null) throw new ArgumentNullException(nameof(item));
-            
-            if (item.Name != Constants.AgedBrie && item.Name != Constants.BackstagePassesToATafkal80EtcConcert)
+
+            if (item.Name == Constants.AgedBrie || item.Name == Constants.BackstagePassesToATafkal80EtcConcert)
             {
-                if (item.Quality > Constants.MinQuantity && item.Name != Constants.SulfurasHandOfRagnaros)
-                    item.Quality -= 1;
+                item.ChangeQuality(1);
+
+                if (item.Name == Constants.BackstagePassesToATafkal80EtcConcert)
+                {
+                    if (item.SellIn < 11)
+                        item.ChangeQuality(1);
+
+                    if (item.SellIn < 6)
+                        item.ChangeQuality(1);
+                }
             }
             else
             {
-                if (item.Quality < Constants.MaxQuantity)
-                {
-                    item.Quality += 1;
-
-                    if (item.Name == Constants.BackstagePassesToATafkal80EtcConcert)
-                    {
-                        if (item.SellIn < 11 && item.Quality < Constants.MaxQuantity)
-                            item.Quality += 1;
-
-                        if (item.SellIn < 6 && item.Quality < Constants.MaxQuantity)
-                            item.Quality += 1;
-                    }
-                }
+                if (item.Name != Constants.SulfurasHandOfRagnaros)
+                    item.ChangeQuality(-1);
             }
 
             if (item.Name != Constants.SulfurasHandOfRagnaros)
                 item.SellIn -= 1;
 
-            if (item.SellIn < Constants.MinQuantity)
+            if (item.SellIn < 0)
                 if (item.Name != Constants.AgedBrie)
                 {
                     if (item.Name != Constants.BackstagePassesToATafkal80EtcConcert)
                     {
-                        if (item.Quality > Constants.MinQuantity && item.Name != Constants.SulfurasHandOfRagnaros)
-                            item.Quality -= 1;
+                        if (item.Name != Constants.SulfurasHandOfRagnaros)
+                            item.ChangeQuality(-1);
                     }
                     else
-                        item.Quality = 0;
+                        item.ChangeQuality(-1 * item.Quality);
                 }
                 else
-                {
-                    if (item.Quality < Constants.MaxQuantity)
-                        item.Quality += 1;
-                }
+                    item.ChangeQuality(1);
         }
     }
 }
---
+++ ItemExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csharp;
+
+public static class ItemExtensions
+{
+    /// <summary>
+    /// Description is required
+    /// </summary>
+    /// <param name="item"></param>
+    /// <param name="qualityChange"></param>
+    public static void ChangeQuality(this Item item, int qualityChange)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+
+        item.Quality = Math.Clamp(item.Quality + qualityChange, Constants.MinQuantity, Constants.MaxQuantity);
+    }
+}
